a82dd6b4e84b93f266cf96c5558fc46d72d5fa8d
unknown <ibrahimalouissa@gmail.com>
2021-07-22 23:07:05 +0200

    Stripe Payment System

---
+++ Website/stripe.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('vendor/autoload.php');
+\Stripe\Stripe::setApiKey('sk_test_WuXk3k5YlhoL4SpYJp90S0nP00pxyIasWP');
+
+$session = \Stripe\Checkout\Session::create([
+  'payment_method_types' => ['card'],
+  'line_items' => [[
+    'price_data' => [
+      'currency' => 'eur',
+      'product_data' => [
+        'name' => 'T-shirt',
+      ],
+      'unit_amount' => 50,
+    ],
+    'quantity' => 1,
+  ]],
+  'mode' => 'payment',
+  'success_url' => 'http://localhost:4242/success',
+  'cancel_url' => 'http://example.com/cancel',
+]);
+
+?>
+
+<html>
+  <head>
+    <title>Buy cool new product</title>
+    <script src="https://js.stripe.com/v3/"></script>
+  </head>
+  <body>
+    <button id="checkout-button">Checkout</button>
+    <script>
+      var stripe = Stripe('pk_test_lFFZ7rdpHQ6jTZkSZpqKPa6K00HkzLPwFr');
+      const btn = document.getElementById("checkout-button")
+      btn.addEventListener('click', function(e) {
+        e.preventDefault();
+        stripe.redirectToCheckout({
+          sessionId: "<?php echo $session->id; ?>"
+        });
+      });
+    </script>
+  </body>
+</html>
---
+++ Website/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9f9a84757cae6656a8b264bec86411ae26cdd3a0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9f9a84757cae6656a8b264bec86411ae26cdd3a0',
+            'dev_requirement' => false,
+        ),
+        'stripe/stripe-php' => array(
+            'pretty_version' => 'v7.89.0',
+            'version' => '7.89.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../stripe/stripe-php',
+            'aliases' => array(),
+            'reference' => 'fea12a15d46d15c017b3fe76f78aa7c983cb5928',
+            'dev_requirement' => false,
+        ),
+    ),
+);
